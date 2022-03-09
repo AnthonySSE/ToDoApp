7efe91ca989084caa542e6f51a3b494d4406ba4b
AnthonySSE <anthonyasse17@gmail.com>
2022-03-08 21:42:18 -0400

    Avances hasta el 08/03/2022
    Se creó el componente ListaTareas el cual reemplaza el componente VerTareas.
    Se creó el servicio ListaTareas.Service.Ts
    Se creó el model ListaTareas.Model.ts
    Se agregaron estilos y otras especificaciones al componentes de crear-tareas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CrearTareasComponent } from './crear-tareas/crear-tareas.component';
-import { VerTareasComponent } from './ver-tareas/ver-tareas.component';
 import { EliminarTareasComponent } from './eliminar-tareas/eliminar-tareas.component';
 import { ActualizarTareasComponent } from './actualizar-tareas/actualizar-tareas.component';
+import { ListaTareasComponent } from './lista-tareas/lista-tareas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CrearTareasComponent,
-    VerTareasComponent,
     EliminarTareasComponent,
-    ActualizarTareasComponent
+    ActualizarTareasComponent,
+    ListaTareasComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/crear-tareas/crear-tareas.component.ts
+++ src/app/crear-tareas/crear-tareas.component.ts
@@ -9,7 +9,6 @@ export class CrearTareasComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
